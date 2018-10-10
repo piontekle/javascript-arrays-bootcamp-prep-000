7b23725c570b00b6f8ed7cfeecb745bed18962e7
Lauren Piontek <piontekle@gmail.com>
2018-10-10 20:17:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function accessElementInArray (array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift();
+  return array.shift();
 }
 
 function removeElementFromBeginningOfArray(array) {
