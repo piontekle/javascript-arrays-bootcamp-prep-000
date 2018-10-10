331caa3ed284e26d450c5a0c6661c7a6fe7d22ce
Lauren Piontek <piontekle@gmail.com>
2018-10-10 19:50:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,5 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+ array.slice(1)
+ return array
 }
