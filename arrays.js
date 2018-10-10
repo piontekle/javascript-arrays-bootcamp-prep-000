b7ac202c20df68a36f865a2303666418d7ee0f71
Lauren Piontek <piontekle@gmail.com>
2018-10-10 19:45:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,8 @@ function destructivelyAddElementToEndOfArray(array,elementToAdd) {
 function accessElementInArray (array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
